82288a0a5f3cf5545cb7e0c5b8f912a0ca9f67fa
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-04-15 10:45:19 +0800

    1

--- OpenAI.Yarp/Program.cs
+++ OpenAI.Yarp/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder
+    .Configuration
+    .AddJsonFile("appsettings.yarp.json")
+    .Build();
+
+builder
     .Services
     .AddReverseProxy()
     .LoadFromConfig
